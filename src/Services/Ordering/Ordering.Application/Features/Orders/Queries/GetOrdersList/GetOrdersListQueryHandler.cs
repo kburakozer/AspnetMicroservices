bb9412e61e9d5aebab3ddc183ddfcccacdaa0680
kburakozer <kburakozer@yahoo.com>
2022-09-18 13:07:54 +0300

    GetOrderQuery handler was implemented

--- src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
 using Ordering.Application.Contracts.Persistence;
 using System;
 using System.Collections.Generic;
@@ -12,8 +13,20 @@ namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
     public  class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
     {
         private readonly IOrderRepository _orderRepository;
-        public Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderRepository, IMapper mapper)
         {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+            var orderList = await _orderRepository.GetOrdersByUserName(request.UserName);
+            var orders = _mapper.Map<List<OrdersVm>>(orderList);
+            return orders;
+
 
         }
     }
