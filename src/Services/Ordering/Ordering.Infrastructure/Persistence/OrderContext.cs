068b5b3a9a6c18429cad58687dd0c33f941cb637
kburakozer <kburakozer@yahoo.com>
2022-10-23 10:32:48 +0300

    Persistence classes were implemented.

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Persistence
@@ -12,6 +15,28 @@ namespace Ordering.Infrastructure.Persistence
         public OrderContext(DbContextOptions<OrderContext> options ) : base( options )
         {
 
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swn";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
